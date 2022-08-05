20f6360cd254f4ec4c3d06fbc9aab0f7588ae9b1
fae1i <minh.pd194803@sis.hust.edu.vn>
2022-08-05 15:20:01 +0700

    update home

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import HeroSection from './home/HeroSection';
 import Footer from './home/Footer';
-import Cards from './home/Cards';
-
 function Home({isLoggedIn}) {
   return (
     <>
       <HeroSection
         isLoggedIn = {isLoggedIn}
       />
-      <Cards />
       <Footer />
     </>
   )
